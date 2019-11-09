4264ad019a70086e833d5af8d42ef38356e58c3c
Roger de Llúria <generacio1octubre@gmail.com>
2019-11-09 12:55:51 +0100

    TTest

--- functions/xat.js
+++ functions/xat.js
@@ -1,7 +1,9 @@
-let TOKEN = process.env.TELEGRAM_BOT_TOKEN
+const Telegraf = require('telegraf')
 
 exports.handler = function(event, context, callback) {
-    console.log(TOKEN);
+    const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
+    const tmp = JSON.parse(event.body);
+    bot.start((ctx) => ctx.reply('Welcome'))
     callback(null, {
         statusCode: 200,
         body: "HelloBOT"
