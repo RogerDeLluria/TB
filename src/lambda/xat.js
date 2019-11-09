d504421a5b7f13c92395caae59d48de8e166b876
Roger de Llúria <generacio1octubre@gmail.com>
2019-11-09 19:13:56 +0100

    Hi

--- src/lambda/xat.js
+++ src/lambda/xat.js
@@ -1,9 +1,13 @@
 const Telegraf = require('telegraf')
+const app = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+app.hears('hi', ctx => ctx.reply('Hey there!'));
 
 exports.handler = function(event, context, callback) {
-    
-    callback(null, {
-        statusCode: 200,
-        body: "HelloBOT"
-    })
+    const tmp = JSON.parse(event.body); // get data passed to us
+    app.handleUpdate(tmp); // make Telegraf process that data
+    return callback(null, { // return something for webhook, so it doesn't try to send same stuff again
+      statusCode: 200,
+      body: '',
+    });
 }
