0943135cb2687195433650c468190904d5d93bb0
Roger de Llúria <generacio1octubre@gmail.com>
2019-11-09 13:32:03 +0100

    Trying

--- src/lambda/xat.js
+++ src/lambda/xat.js
@@ -1,9 +1,7 @@
 const Telegraf = require('telegraf')
 
 exports.handler = function(event, context, callback) {
-    const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
-    const tmp = JSON.parse(event.body);
-    bot.start((ctx) => ctx.reply('Welcome'))
+    
     callback(null, {
         statusCode: 200,
         body: "HelloBOT"
